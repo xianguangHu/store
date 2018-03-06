12a2841c6a548a72cd96df9b7f6f753329c7facb
huxianguang <15789762@qq.com>
2018-03-06 17:44:08 +0800

    2018年3月6日17:43:38

---
+++ cloud-config/src/main/java/com/guang/CloudConfigApplication.java
@@ -0,0 +1,16 @@
+package com.guang;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 17721
+ */
+@SpringBootApplication
+public class CloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigApplication.class,args);
+    }
+}
---
+++ web-admin/src/main/java/com/guang/WebAdminApplication.java
@@ -0,0 +1,16 @@
+package com.guang;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 17721
+ */
+@SpringBootApplication
+public class WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminApplication.class,args);
+    }
+}
