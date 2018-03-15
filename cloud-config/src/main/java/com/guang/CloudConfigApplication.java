e67d016b21df9316cbf21b3eb9bac8469624a028
huxianguang <15789762@qq.com>
2018-03-15 16:55:07 +0800

    2018年3月15日16:55:00

--- cloud-config/src/main/java/com/guang/CloudConfigApplication.java
+++ cloud-config/src/main/java/com/guang/CloudConfigApplication.java
@@ -3,11 +3,15 @@ package com.guang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 17721
  */
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class CloudConfigApplication {
 
     public static void main(String[] args) {
--- web-admin/src/main/java/com/guang/WebAdminApplication.java
+++ web-admin/src/main/java/com/guang/WebAdminApplication.java
@@ -1,11 +1,14 @@
 package com.guang;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 17721
@@ -14,9 +17,20 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrixDashboard
+@RestController
 public class WebAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebAdminApplication.class,args);
+    }
+
+
+    @Value("${foo}")
+    String foo;
+
+
+    @RequestMapping(value = "/foo")
+    public String foo() {
+        return foo;
     }
 }
